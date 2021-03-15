f9faf669658f9e8bd2855c25e4178501c50362a4
Bartosz <radu480@o2.pl>
2021-03-15 23:40:38 +0100

    Prepared infrastructure stuff and profiles of automapper.

---
+++ Foodie.Meals/Profiles/MealsProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Foodie.Meals.Commands.Meals;
+using Foodie.Meals.Models;
+using Foodie.Meals.Responses.Meals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Profiles
+{
+    public class MealsProfile : Profile
+    {
+        public MealsProfile()
+        {
+            CreateMap<Meal, MealResponse>();
+            CreateMap<CreateMealCommand, Meal>();
+            CreateMap<EditMealCommand, Meal>()
+                .ForMember(x => x.MealId, opt => opt.Ignore());
+        }
+    }
+}
---
+++ Foodie.Meals/Profiles/RestaurantsProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Foodie.Meals.Commands.Restaurants;
+using Foodie.Meals.Models;
+using Foodie.Meals.Responses.Restaurants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Profiles
+{
+    public class RestaurantsProfile : Profile
+    {
+        public RestaurantsProfile()
+        {
+            CreateMap<Restaurant, RestaurantResponse>();
+            CreateMap<CreateRestaurantCommand, Restaurant>();
+            CreateMap<EditRestaurantCommand, Restaurant>()
+                .ForMember(x => x.RestaurantId, opt => opt.Ignore());
+        }
+    }
+}
