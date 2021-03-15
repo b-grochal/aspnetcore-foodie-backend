e45fac1fd8ecf4f2cf2eff2290a180417a6677a9
Bartosz <radu480@o2.pl>
2021-03-15 23:29:49 +0100

    Prepared handlers and queries for meals.

---
+++ Foodie.Meals/Queries/Meals/GetAllMealsQuery.cs
@@ -0,0 +1,13 @@
+﻿using Foodie.Meals.Responses.Meals;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Queries.Meals
+{
+    public class GetAllMealsQuery : IRequest<IEnumerable<MealResponse>>
+    {
+    }
+}
