953aa82a7b3365e897d562613dd0e71980004bf9
Bartosz <radu480@o2.pl>
2021-03-15 23:15:57 +0100

    Prepared commands.

---
+++ Foodie.Meals/Commands/Meals/CreateMealCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Commands.Meals
+{
+    public class CreateMealCommand : IRequest
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
