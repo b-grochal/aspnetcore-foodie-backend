f72600949b68b6750696a2b9919fe3da627771c2
Bartosz <radu480@o2.pl>
2021-03-16 16:17:43 +0100

    Prepared responses.

---
+++ Foodie.Identity/Responses/Users/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Identity.Responses.Users
+{
+    public class UserResponse
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
