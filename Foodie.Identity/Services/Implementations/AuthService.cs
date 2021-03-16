97c3bb77175f248ac582ec1a78a0bdc1acbbf901
Bartosz <radu480@o2.pl>
2021-03-16 16:34:12 +0100

    Prepared services with infrastructure stuff.

---
+++ Foodie.Identity/Services/Implementations/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using Foodie.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Identity.Services.Implementations
+{
+    public class AuthService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AuthService(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<ApplicationUser> AuthenticateUser(string email, string password)
+        {
+            var applicationUser = await userManager.FindByEmailAsync(email);
+            var isPasswordValid = await userManager.CheckPasswordAsync(applicationUser, password);
+
+            if (applicationUser != null && isPasswordValid)
+            {
+                return applicationUser;
+            }
+            return null;
+        }
+    }
+}
