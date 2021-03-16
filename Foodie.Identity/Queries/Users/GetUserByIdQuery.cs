a8e859c1f830e33e2aee21b1339f8ebac50def50
Bartosz <radu480@o2.pl>
2021-03-16 22:26:41 +0100

    Prepared queries.

---
+++ Foodie.Identity/Queries/Users/GetUserByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using Foodie.Identity.Responses.Users;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Identity.Queries.Users
+{
+    public class GetUserByIdQuery : IRequest<UserResponse>
+    {
+        public string Id { get; set; }
+
+        public GetUserByIdQuery(string id)
+        {
+            this.Id = id;
+        }
+    }
+}
