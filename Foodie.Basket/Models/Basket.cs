733c5c23e9a2a303135b956fbfa94a005f7479be
Bartosz <radu480@o2.pl>
2021-04-22 00:22:15 +0200

    Created model for basket API.

---
+++ Foodie.Basket/Models/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Basket.Models
+{
+    public class Basket
+    {
+        public string UserId { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+        public Basket()
+        {
+                
+        }
+
+        public Basket(string userId)
+        {
+            this.UserId = userId;
+        }
+    }
+}
