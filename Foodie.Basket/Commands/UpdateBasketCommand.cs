9c218a9770b74477f0934cbd5c9bb38051895cde
Bartosz <radu480@o2.pl>
2022-02-12 16:00:31 +0100

    Prepared controller actions for basket.

---
+++ Foodie.Basket/Commands/UpdateBasketCommand.cs
@@ -0,0 +1,14 @@
+﻿using Foodie.Basket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Basket.Commands
+{
+    public class UpdateBasketCommand
+    {
+        public string UserId { get; set; }
+        public List<BasketItem> Items { get; set; }
+    }
+}
--- Foodie.Basket/Controllers/BasketController.cs
+++ Foodie.Basket/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Foodie.Basket.Commands;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,29 @@ namespace Foodie.Basket.Controllers
     [ApiController]
     public class BasketController : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBasket([FromBody] UpdateBasketCommand updateBasketCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasket()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("checkout")]
+        [HttpPost]
+        public async Task<ActionResult> CheckoutAsync([FromBody] CheckoutBasketCommand checkoutBasketCommand)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
