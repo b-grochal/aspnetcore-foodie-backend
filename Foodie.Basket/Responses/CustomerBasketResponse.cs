af780a57c9343b6601cc50a8591ab78be5e14672
Bartosz <radu480@o2.pl>
2022-02-14 00:29:48 +0100

    Prepare responses for basket service.

---
+++ Foodie.Basket/Responses/CustomerBasketResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodie.Basket.Responses
+{
+    public class CustomerBasketResponse
+    {
+        public string UserId { get; set; }
+        public List<BasketItemResponse> Items { get; set; }
+    }
+}
