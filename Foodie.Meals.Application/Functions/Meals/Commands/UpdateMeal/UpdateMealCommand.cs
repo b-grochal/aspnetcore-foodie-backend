962fea6147e64739b22b34bf32cb699ed864f93d
Bartosz Grochal <radu480@outlook.com>
2022-03-14 20:04:57 +0100

    Implement update command for meals.

---
+++ Foodie.Meals.Application/Functions/Meals/Commands/UpdateMeal/UpdateMealCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Application.Functions.Meals.Commands.UpdateMeal
+{
+    public class UpdateMealCommand
+    {
+        public int MealId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
---
+++ Foodie.Meals.Application/Functions/Meals/Commands/UpdateMeal/UpdateMealCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Foodie.Meals.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Application.Functions.Meals.Commands.UpdateMeal
+{
+    public class UpdateMealCommandHandler : IRequestHandler<UpdateMealCommand>
+    {
+        private readonly IMealsRepository mealsRepository;
+        private readonly IMapper mapper;
+
+        public UpdateMealCommandHandler(IMealsRepository mealsRepository, IMapper mapper)
+        {
+            this.mealsRepository = mealsRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateMealCommand request, CancellationToken cancellationToken)
+        {
+            var meal = await mealsRepository.GetMeal(request.MealId);
+            var editedMeal = mapper.Map(request, meal);
+            await mealsRepository.EditMeal(editedMeal);
+            return new Unit();
+        }
+    }
+}
