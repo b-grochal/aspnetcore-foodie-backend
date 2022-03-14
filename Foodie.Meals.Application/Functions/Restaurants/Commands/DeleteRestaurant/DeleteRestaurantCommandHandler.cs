d003a2d9406019925e552f573b65d553e6695845
Bartosz Grochal <radu480@outlook.com>
2022-03-14 20:36:16 +0100

    Create command for deleting restaurant.

---
+++ Foodie.Meals.Application/Functions/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Foodie.Meals.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Application.Functions.Restaurants.Commands.DeleteRestaurant
+{
+    public class DeleteRestaurantCommandHandler : IRequestHandler<DeleteRestaurantCommand>
+    {
+        private readonly IRestaurantsRepository restaurantRepository;
+
+        public DeleteRestaurantCommandHandler(IRestaurantsRepository restaurantRepository)
+        {
+            this.restaurantRepository = restaurantRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteRestaurantCommand request, CancellationToken cancellationToken)
+        {
+            await restaurantRepository.DeleteRestaurant(request.RestaurantId);
+            return new Unit();
+        }
+    }
+}
