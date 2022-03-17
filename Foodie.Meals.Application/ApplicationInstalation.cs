9a92a094d608e6842184f8ab49144ad800889ef7
Bartosz Grochal <radu480@outlook.com>
2022-03-18 00:50:26 +0100

    Prepare installer for application layer.

---
+++ Foodie.Meals.Application/ApplicationInstalation.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Application
+{
+    public static class ApplicationInstalation
+    {
+        public static IServiceCollection AddEUniversityApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
