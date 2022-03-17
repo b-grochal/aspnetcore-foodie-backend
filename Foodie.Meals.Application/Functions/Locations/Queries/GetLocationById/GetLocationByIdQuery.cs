d78b2a333dc7992163d9d280c80a49c30fad37cc
Bartosz Grochal <radu480@outlook.com>
2022-03-18 00:35:56 +0100

    Implement query for fetching location by id.

---
+++ Foodie.Meals.Application/Functions/Locations/Queries/GetLocationById/GetLocationByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Application.Functions.Locations.Queries.GetLocationById
+{
+    public class GetLocationByIdQuery : IRequest<LocationDetailsResponse>
+    {
+        public int LocationId { get; }
+
+        public GetLocationByIdQuery(int locationId)
+        {
+            this.LocationId = locationId;
+        }
+    }
+}
---
+++ Foodie.Meals.Application/Functions/Locations/Queries/GetLocationById/GetLocationByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Foodie.Meals.Application.Contracts.Infrastructure.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Application.Functions.Locations.Queries.GetLocationById
+{
+    public class GetLocationByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, LocationDetailsResponse>
+    {
+        private readonly ILocationsRepository locationsRepository;
+        private readonly IMapper mapper;
+
+        public GetLocationByIdQueryHandler(ILocationsRepository locationsRepository, IMapper mapper)
+        {
+            this.locationsRepository = locationsRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<LocationDetailsResponse> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+        {
+            var location = await locationsRepository.GetLocation(request.LocationId);
+            return mapper.Map<LocationDetailsResponse>(location);
+        }
+    }
+}
