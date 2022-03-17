10e54659b7ab60f929ad0d5d7897f5840c457b60
Bartosz Grochal <radu480@outlook.com>
2022-03-17 23:53:50 +0100

    Created base query for paging purposes.

--- Foodie.Meals.Application/Functions/Restaurants/Queries/GetRestaurants/GetRestaurantsQuery.cs
+++ Foodie.Meals.Application/Functions/Restaurants/Queries/GetRestaurants/GetRestaurantsQuery.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Foodie.Shared.Queries;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,22 +8,8 @@ using System.Threading.Tasks;
 
 namespace Foodie.Meals.Application.Functions.Restaurants.Queries.GetRestaurants
 {
-    public class GetRestaurantsQuery : IRequest<RestaurantsListResponse>
+    public class GetRestaurantsQuery : PagedQuery, IRequest<RestaurantsListResponse>
     {
-        const int maxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize = 10;
-        public int PageSize
-        {
-            get
-            {
-                return _pageSize;
-            }
-            set
-            {
-                _pageSize = (value > maxPageSize) ? maxPageSize : value;
-            }
-        }
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public string CityName { get; set; }
