b2881e9ae05ded1f074ece56acc58ff91da9bb79
Bartosz Grochal <radu480@outlook.com>
2022-03-21 23:20:16 +0100

    Prepare context for database.

--- Foodie.Meals.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Foodie.Meals.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ Foodie.Meals.Infrastructure/MealsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Foodie.Meals.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.Infrastructure
+{
+    public class MealsDbContext : DbContext
+    {
+        public MealsDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
