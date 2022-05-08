fddc51abf32a8d55829e31ec212b3974857a0b6b
Bartosz Grochal <radu480@outlook.com>
2022-05-08 22:24:47 +0200

    Prepare mock of mediator for cities.

---
+++ Foodie.Meals.UnitTests/Mocks/Mediators/MockMediatorForCities.cs
@@ -0,0 +1,150 @@
+﻿using Foodie.Meals.Application.Functions.Cities.Commands.CreateCity;
+using Foodie.Meals.Application.Functions.Cities.Commands.DeleteCity;
+using Foodie.Meals.Application.Functions.Cities.Commands.UpdateCity;
+using Foodie.Meals.Application.Functions.Cities.Queries.GetCities;
+using Foodie.Meals.Application.Functions.Cities.Queries.GetCityById;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Meals.UnitTests.Mocks.Mediators
+{
+    public class MockMediatorForCities : Mock<IMediator>
+    {
+        public MockMediatorForCities MockSendingCreateCityCommand()
+        {
+            Setup(m => m.Send(It.IsAny<CreateCityCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync((CreateCityCommand createCityCommand, CancellationToken cancellationToken) =>
+            {
+                return new CreateCityCommandResponse
+                {
+                    CityId = 1,
+                    Name = createCityCommand.Name,
+                    Country = createCityCommand.Country
+                };
+            });
+
+            return this;
+        }
+
+        public MockMediatorForCities VerifySendingCreateCityCommand(Times times)
+        {
+            Verify(m => m.Send(It.IsAny<CreateCityCommand>(), It.IsAny<CancellationToken>()), times);
+
+            return this;
+        }
+
+        public MockMediatorForCities MockSendingUpdateCityCommand()
+        {
+            Setup(m => m.Send(It.IsAny<UpdateCityCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync((UpdateCityCommand updateCityCommand, CancellationToken cancellationToken) =>
+            {
+                return new UpdateCityCommandResponse
+                {
+                    CityId = updateCityCommand.CityId,
+                    Name = updateCityCommand.Name,
+                    Country = updateCityCommand.Country
+                };
+            });
+
+            return this;
+        }
+
+        public MockMediatorForCities VerifySendingUpdateCityCommand(Times times)
+        {
+            Verify(m => m.Send(It.IsAny<UpdateCityCommand>(), It.IsAny<CancellationToken>()), times);
+
+            return this;
+        }
+
+        public MockMediatorForCities MockSendingDeleteCityCommand()
+        {
+            Setup(m => m.Send(It.IsAny<DeleteCityCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync((DeleteCityCommand deleteCityCommand, CancellationToken cancellationToken) =>
+            {
+                return new DeleteCityCommandResponse
+                {
+                    CityId = deleteCityCommand.CityId,
+                };
+            });
+
+            return this;
+        }
+
+        public MockMediatorForCities VerifySendingDeleteCityCommand(Times times)
+        {
+            Verify(m => m.Send(It.IsAny<DeleteCityCommand>(), It.IsAny<CancellationToken>()), times);
+
+            return this;
+        }
+
+        public MockMediatorForCities MockSendingGetCityByIdQuery()
+        {
+            Setup(m => m.Send(It.IsAny<GetCityByIdQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync((GetCityByIdQuery getCityByIdQuery, CancellationToken cancellationToken) =>
+            {
+                return new CityDetailsResponse
+                {
+                    CityId = getCityByIdQuery.CityId,
+                    Name = "Test category",
+                    Country = "Test country"
+                };
+            });
+
+            return this;
+        }
+
+        public MockMediatorForCities VerifySendingGetCityByIdQuery(Times times)
+        {
+            Verify(m => m.Send(It.IsAny<GetCityByIdQuery>(), It.IsAny<CancellationToken>()), times);
+
+            return this;
+        }
+
+        public MockMediatorForCities MockSendingGetCitiesQuery()
+        {
+            Setup(m => m.Send(It.IsAny<GetCitiesQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync((GetCitiesQuery getCitiesQuery, CancellationToken cancellationToken) =>
+            {
+                return new CitiesListResponse
+                {
+                    Name = getCitiesQuery.Name,
+                    Country = getCitiesQuery.Country,
+                    PageSize = getCitiesQuery.PageSize,
+                    CurrentPage = getCitiesQuery.PageNumber,
+                    TotalPages = 2,
+                    Cities = new List<CityResponse>
+                    {
+                        new CityResponse
+                        {
+                            CityId = 1,
+                            Name = "Test city 1",
+                            Country = "Test country"
+                        },
+                        new CityResponse
+                        {
+                            CityId = 2,
+                            Name = "Test city 2",
+                            Country = "Test country"
+                        },
+                        new CityResponse
+                        {
+                            CityId = 3,
+                            Name = "Test city 3",
+                            Country = "Test country"
+                        }
+                    }
+                };
+            });
+
+            return this;
+        }
+
+        public MockMediatorForCities VerifySendingGetCitiesQuery(Times times)
+        {
+            Verify(m => m.Send(It.IsAny<GetCitiesQuery>(), It.IsAny<CancellationToken>()), times);
+
+            return this;
+        }
+    }
+}
