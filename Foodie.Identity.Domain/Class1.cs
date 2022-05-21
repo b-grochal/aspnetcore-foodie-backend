8ab488c3df27fd7aba1ea832ae485efab123a9d8
Bartosz Grochal <radu480@outlook.com>
2022-05-21 19:34:34 +0200

    Prepare domian for service of identity.

--- Foodie.Identity.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Foodie.Identity.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ Foodie.Identity.Domain/Exceptions/OrderHandlerNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using Foodie.Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Identity.Domain.Exceptions
+{
+    public class OrderHandlerNotFoundException : NotFoundException
+    {
+        public OrderHandlerNotFoundException(string orderHandlerId) : base($"The order handler with the indetifier {orderHandlerId} was not found.") { }
+    }
+}
