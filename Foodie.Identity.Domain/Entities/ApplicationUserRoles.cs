bc443b44fc3f57492bcfdf2803382dbc4886cb64
Bartosz Grochal <radu480@outlook.com>
2022-05-21 19:36:24 +0200

    Add user's roles. Remove domain from API layer.

--- Foodie.Identity.Domain/Entities/ApplicationUserRoles.cs
+++ Foodie.Identity.Domain/Entities/ApplicationUserRoles.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace Foodie.Identity.Models
+namespace Foodie.Identity.Domain.Entities
 {
     public class ApplicationUserRoles
     {
         public const string Admin = "Admin";
         public const string OrderHandler = "OrderHandler";
-        public const string User = "User";
+        public const string Customer = "Customer";
     }
 }
