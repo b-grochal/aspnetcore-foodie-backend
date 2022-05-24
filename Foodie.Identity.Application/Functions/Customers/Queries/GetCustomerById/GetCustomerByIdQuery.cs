71fc2ddc05682d0e9c5522c7a40b781de9834225
Bartosz Grochal <radu480@outlook.com>
2022-05-24 12:31:49 +0200

    Prepare fetching admins, customers and order handlers by id.

---
+++ Foodie.Identity.Application/Functions/Customers/Queries/GetCustomerById/GetCustomerByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Identity.Application.Functions.Customers.Queries.GetCustomerById
+{
+    public class GetCustomerByIdQuery : IRequest<GetCustomerByIdQueryResponse>
+    {
+        public string CustomerId { get; }
+
+        public GetCustomerByIdQuery(string customerId)
+        {
+            this.CustomerId = customerId;
+        }
+    }
+}
---
+++ Foodie.Identity.Application/Functions/Customers/Queries/GetCustomerById/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Foodie.Identity.Application.Contracts.Infrastructure.Repositories;
+using Foodie.Identity.Domain.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Identity.Application.Functions.Customers.Queries.GetCustomerById
+{
+    public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, GetCustomerByIdQueryResponse>
+    {
+        private readonly ICustomersRepository customersRepository;
+        private readonly IMapper mapper;
+
+        public GetCustomerByIdQueryHandler(ICustomersRepository customersRepository, IMapper mapper)
+        {
+            this.customersRepository = customersRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<GetCustomerByIdQueryResponse> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var customer = await customersRepository.GetByIdAsync(request.CustomerId);
+
+            if (customer == null)
+                throw new CustomerNotFoundException(request.CustomerId);
+
+            return mapper.Map<GetCustomerByIdQueryResponse>(customer);
+        }
+    }
+}
