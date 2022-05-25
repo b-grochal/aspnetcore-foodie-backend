f84d9e83ec895c258220bbf17af29db326695f95
Bartosz Grochal <radu480@outlook.com>
2022-05-25 11:44:49 +0200

    Prepare function for signing up.

---
+++ Foodie.Identity.Application/Functions/Auth/Commands/SignUp/SIgnUpCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Foodie.Identity.Application.Contracts.Infrastructure.Repositories;
+using Foodie.Identity.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Identity.Application.Functions.Auth.Commands.SignUp
+{
+    public class SignUpCommandHandler : IRequestHandler<SignUpCommand, SignUpCommandResponse>
+    {
+        private readonly ICustomersRepository customersRepository;
+        private readonly IMapper mapper;
+
+        public SignUpCommandHandler(ICustomersRepository customersRepository, IMapper mapper)
+        {
+            this.customersRepository = customersRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<SignUpCommandResponse> Handle(SignUpCommand request, CancellationToken cancellationToken)
+        {
+            var customer = mapper.Map<Customer>(request);
+            await customersRepository.CreateAsync(customer);
+            return mapper.Map<SignUpCommandResponse>(customer);
+        }
+    }
+}
