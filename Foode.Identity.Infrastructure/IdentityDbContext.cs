31d7efa3d07932c7b8a37582e39c6e9fe8aea8e6
Bartosz Grochal <radu480@outlook.com>
2022-05-26 22:23:55 +0200

    Prepare context for identity service.

---
+++ Foode.Identity.Infrastructure/IdentityDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Foodie.Identity.Domain.Entities;
+using Foodie.Shared.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foode.Identity.Infrastructure
+{
+    public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<OrderHandler> OrderHandlers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public IdentityDbContext(DbContextOptions options) : base(options) { }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Admin>().ToTable("Admins");
+            modelBuilder.Entity<OrderHandler>().ToTable("OrderHandlers");
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+        }
+    }
+}
---
+++ Foode.Identity.Infrastructure/Repositories/AdminsRepository.cs
@@ -0,0 +1,54 @@
+﻿using Foodie.Identity.Application.Contracts.Infrastructure.Repositories;
+using Foodie.Identity.Domain.Entities;
+using Foodie.Shared.Extensions;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foode.Identity.Infrastructure.Repositories
+{
+    public class AdminsRepository : IAdminsRepository
+    {
+        private readonly UserManager<Admin> userManager;
+        private readonly
+
+        public AdminsRepository(UserManager<Admin> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task CreateAsync(Admin admin)
+        {
+            await userManager.CreateAsync(admin);
+            await userManager.AddToRoleAsync(admin, ApplicationUserRoles.Admin);
+        }
+
+        public async Task DeleteAsync(Admin admin)
+        {
+            await userManager.DeleteAsync(admin);
+        }
+
+        public async Task<IReadOnlyList<Admin>> GetAllAsync()
+        {
+            return (IReadOnlyList<Admin>)await userManager.GetUsersInRoleAsync(ApplicationUserRoles.Admin);
+        }
+
+        public async Task<PagedList<Admin>> GetAllAsync(int pageNumber, int pageSize, string email)
+        {
+            return (PagedList<Admin>)await userManager.GetUsersInRoleAsync(ApplicationUserRoles.Admin);
+        }
+
+        public async Task<Admin> GetByIdAsync(string id)
+        {
+            return await userManager.FindByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(Admin admin)
+        {
+            await userManager.UpdateAsync(admin);
+        }
+    }
+}
