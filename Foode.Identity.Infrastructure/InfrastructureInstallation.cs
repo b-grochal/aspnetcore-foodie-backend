527a2748282538fa8e6d1135c693e47e8d1fc06a
Bartosz Grochal <radu480@outlook.com>
2022-05-30 19:40:11 +0200

    Prepare installator for infrastructure layer.

--- Foode.Identity.Infrastructure/InfrastructureInstallation.cs
+++ Foode.Identity.Infrastructure/InfrastructureInstallation.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Foode.Identity.Infrastructure.Repositories;
+using Foode.Identity.Infrastructure.Services;
+using Foodie.Identity.Application.Contracts.Infrastructure.Repositories;
+using Foodie.Identity.Application.Contracts.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,20 @@ using System.Threading.Tasks;
 
 namespace Foode.Identity.Infrastructure
 {
-    internal class InfrastructureInstallation
+    public static class InfrastructureInstallation
     {
+        public static IServiceCollection AddMealsInfrastructure(this IServiceCollection services, Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DbConnection")));
+
+            services.AddScoped<IAdminsRepository, AdminsRepository>();
+            services.AddScoped<ICustomersRepository, CustomersRepository>();
+            services.AddScoped<IOrderHandlersRepository, OrderHandlersRepository>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IJwtService, JwtService>();
+
+            return services;
+        }
     }
 }
