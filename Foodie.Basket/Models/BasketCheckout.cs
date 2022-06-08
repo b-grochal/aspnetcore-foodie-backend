887b35f7c74e3023e2b6da9838af5731c6d17746
Bartosz Grochal <radu480@outlook.com>
2022-06-08 12:18:10 +0200

    Update model.

--- Foodie.Basket/Models/BasketCheckout.cs
+++ Foodie.Basket/Models/BasketCheckout.cs
@@ -7,11 +7,6 @@ namespace Foodie.Basket.Models
 {
     public class BasketCheckout
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
+        public string Address { get; set; }
     }
 }
