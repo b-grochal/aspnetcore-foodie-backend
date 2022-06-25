48c52c6d76a3d7b28fbdafc7b68b751e30807560
Bartosz Grochal <radu480@outlook.com>
2022-06-25 23:58:39 +0200

    Prepare some missing contracts.

--- Foodie.Orders.Application/Contracts/Infrastructure/Context/IUnitOfWork.cs
+++ Foodie.Orders.Application/Contracts/Infrastructure/Context/IUnitOfWork.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Foodie.Orders.Application.Contracts.Infrastructure.Repositories
+namespace Foodie.Orders.Application.Contracts.Infrastructure.Context
 {
     public interface IUnitOfWork : IDisposable
     {
---
+++ Foodie.Orders.Application/Contracts/Infrastructure/Queries/IOrderQueries.cs
@@ -0,0 +1,15 @@
+﻿using Foodie.Orders.Domain.AggregatesModel.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Orders.Application.Contracts.Infrastructure.Queries
+{
+    internal interface IOrderQueries
+    {
+        Task<Order> GetByIdAsync(int id);
+        Task<IEnumerable<Order>> GetAllAsync(string userId);
+    }
+}
