08bc8a9580d6f9cac715a6c266598f15badff8f4
Bartosz Grochal <radu480@outlook.com>
2022-06-26 13:26:30 +0200

    Create new aggregate.

--- Foodie.Orders.Domain/AggregatesModel/BuyerAggregate/Buyer.cs
+++ Foodie.Orders.Domain/AggregatesModel/BuyerAggregate/Buyer.cs
@@ -9,15 +9,15 @@ namespace Foodie.Orders.Domain.AggregatesModel.BuyerAggregate
 {
     public class Buyer : Entity, IAggregateRoot
     {
-        public string IdentityGuid { get; private set; }
+        public string UserId { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public string PhoneNumber { get; private set; }
         public string Email { get; private set; }
 
-        public Buyer(string identity, string firstName, string lastName, string phoneNumber, string email)
+        public Buyer(string userId, string firstName, string lastName, string phoneNumber, string email)
         {
-            IdentityGuid = !string.IsNullOrWhiteSpace(identity) ? identity : throw new ArgumentNullException(nameof(identity));
+            UserId = !string.IsNullOrWhiteSpace(userId) ? userId : throw new ArgumentNullException(nameof(userId));
             FirstName = !string.IsNullOrWhiteSpace(firstName) ? firstName : throw new ArgumentNullException(nameof(firstName));
             LastName = !string.IsNullOrWhiteSpace(lastName) ? lastName : throw new ArgumentNullException(nameof(lastName));
             PhoneNumber = !string.IsNullOrWhiteSpace(phoneNumber) ? phoneNumber : throw new ArgumentNullException(nameof(phoneNumber));
---
+++ Foodie.Orders.Domain/AggregatesModel/ContractorAggregate/Contractor.cs
@@ -0,0 +1,35 @@
+﻿using Foodie.Orders.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Orders.Domain.AggregatesModel.ContractorAggregate
+{
+    public class Contractor : Entity, IAggregateRoot
+    {
+        public int RestaurantId { get; private set; }
+        public string RestaurantName { get; private set; }
+        public int LocationId { get; private set; }
+        public string LocationAddress { get; private set; }
+        public string LocationPhoneNumber { get; private set; }
+        public string LocationEmail { get; private set; }
+        public int CityId { get; private set; }
+        public string CityName { get; private set; }
+        public string Country { get; private set; }
+
+        public Contractor(int restaurantId, string restaurantName, int locationId, string locationAddress, string locationPhoneNumber, string locationEmail, int cityId, string cityName, string country)
+        {
+            RestaurantId = restaurantId > 0 ? restaurantId : throw new ArgumentNullException(nameof(restaurantId));
+            RestaurantName = !string.IsNullOrWhiteSpace(restaurantName) ? restaurantName : throw new ArgumentNullException(nameof(restaurantName));
+            LocationId = locationId > 0 ? locationId : throw new ArgumentNullException(nameof(locationId));
+            LocationAddress = !string.IsNullOrWhiteSpace(locationAddress) ? locationAddress : throw new ArgumentNullException(nameof(locationAddress));
+            LocationPhoneNumber = !string.IsNullOrWhiteSpace(locationPhoneNumber) ? locationPhoneNumber : throw new ArgumentNullException(nameof(locationPhoneNumber));
+            LocationEmail = !string.IsNullOrWhiteSpace(locationEmail) ? locationEmail : throw new ArgumentNullException(nameof(locationEmail));
+            CityId = cityId > 0 ? cityId : throw new ArgumentNullException(nameof(cityId));
+            CityName = !string.IsNullOrWhiteSpace(cityName) ? cityName : throw new ArgumentNullException(nameof(cityName));
+            Country = !string.IsNullOrWhiteSpace(country) ? country : throw new ArgumentNullException(nameof(country));
+        }
+    }
+}
