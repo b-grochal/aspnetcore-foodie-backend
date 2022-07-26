63fa191deed2402512d67f28c8a385d099477314
Bartosz Grochal <radu480@outlook.com>
2022-07-26 23:02:46 +0200

    Installed all necessary packeges for infrastructure of orders.

---
+++ Foodie.Orders.Infrastructure/Configurations/OrderEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Foodie.Orders.Domain.AggregatesModel.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Orders.Infrastructure.Configurations
+{
+    public class OrderEntityTypeConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
