b9bac18d7a82a099fafd47f9e6cb82bebab6a5f2
Bartosz Grochal <radu480@outlook.com>
2022-07-27 22:30:02 +0200

    Prepare context for orders.

---
+++ Foodie.Orders.Infrastructure/OrdersDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Foodie.Orders.Application.Contracts.Infrastructure.Context;
+using Foodie.Orders.Domain.AggregatesModel.BuyerAggregate;
+using Foodie.Orders.Domain.AggregatesModel.ContractorAggregate;
+using Foodie.Orders.Domain.AggregatesModel.OrderAggregate;
+using Foodie.Orders.Infrastructure.Extensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Orders.Infrastructure
+{
+    public class OrdersDbContext : DbContext, IUnitOfWork 
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrdersItem { get; set; }
+        public DbSet<Buyer> Buyers { get; set; }
+        public DbSet<Contractor> Contractors { get; set; }
+
+        private readonly IMediator _mediator;
+
+        public OrdersDbContext(DbContextOptions<OrdersDbContext> options) : base(options) { }
+
+        public OrdersDbContext(DbContextOptions<OrdersDbContext> options, IMediator mediator) : base(options)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            await _mediator.DispatchDomainEventsAsync(this);
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
