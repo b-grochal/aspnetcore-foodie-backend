cd50acc62a670a291ed96f559d9bf82ba88e0f54
Bartosz Grochal <radu480@outlook.com>
2022-09-05 22:35:40 +0200

    Fix contractor.

---
+++ Foodie.Orders.Infrastructure/Migrations/20220905203306_ContractorUpdate.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Foodie.Orders.Infrastructure.Migrations
+{
+    public partial class ContractorUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Contractors_BuyerId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ContractorId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ContractorId",
+                table: "Orders",
+                column: "ContractorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Contractors_ContractorId",
+                table: "Orders",
+                column: "ContractorId",
+                principalTable: "Contractors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Contractors_ContractorId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ContractorId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ContractorId",
+                table: "Orders");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Contractors_BuyerId",
+                table: "Orders",
+                column: "BuyerId",
+                principalTable: "Contractors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
