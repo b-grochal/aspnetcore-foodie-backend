6ac5f67f4fb7aedaa1399285f4219731ddde5262
Bartosz Grochal <radu480@outlook.com>
2022-09-24 20:45:43 +0200

    Prepare mapping profile for orders microservice.

---
+++ Foodie.Orders.Application/Mapper/MapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Foodie.Orders.Application.Contracts.Infrastructure.Queries;
+using Foodie.Orders.Application.Functions.Orders.Queries.GetOrderById;
+using Foodie.Orders.Application.Functions.Orders.Queries.GetOrders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Orders.Application.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            // Orders
+            CreateMap<OrderDetailsQueryDto, GetOrderByIdQueryResponse>();
+            CreateMap<OrderItemQueryDto, OrderItemDto>();
+            CreateMap<OrderQueryDto, OrderDto>();
+        }
+    }
+}
