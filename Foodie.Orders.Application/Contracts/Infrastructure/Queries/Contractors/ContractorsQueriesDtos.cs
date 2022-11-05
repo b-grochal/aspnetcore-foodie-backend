912060ef02eac93b4ab12e8dfa357226320b7e1b
Bartosz Grochal <radu480@outlook.com>
2022-11-05 23:36:51 +0100

    Prepared function for getting contractor by id.

---
+++ Foodie.Orders.Application/Contracts/Infrastructure/Queries/Contractors/ContractorsQueriesDtos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Orders.Application.Contracts.Infrastructure.Queries.Contractors
+{
+    public record ContractorQueryDto
+    {
+        public int ContractorId { get; set; }
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+        public string City { get; private set; }
+        public string Country { get; private set; }
+    }
+
+    public record ContractorDetailsQueryDto
+    {
+        public int ContractorId { get; set; }
+        public int RestaurantId { get; private set; }
+        public string Name { get; private set; }
+        public int LocationId { get; private set; }
+        public string Address { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+        public int CityId { get; private set; }
+        public string City { get; private set; }
+        public string Country { get; private set; }
+    }
+}
