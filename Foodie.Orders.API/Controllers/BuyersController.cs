f0abb02ba76a41f546926fe4bc48f4d900463129
Bartosz Grochal <radu480@outlook.com>
2022-11-08 21:19:21 +0100

    Prepared endpoints and queries for getting info about buyers.

---
+++ Foodie.Orders.API/Controllers/BuyersController.cs
@@ -0,0 +1,37 @@
+﻿using Foodie.Orders.Application.Functions.Buyers.Queries.GetBuyerById;
+using Foodie.Orders.Application.Functions.Buyers.Queries.GetBuyers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Foodie.Orders.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuyersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BuyersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET api/buyers/5
+        [HttpGet("{buyerId}")]
+        public async Task<IActionResult> GetBuyer(int buyerId)
+        {
+            var query = new GetBuyerByIdQuery(buyerId);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        // GET api/buyers
+        [HttpGet]
+        public async Task<IActionResult> GetBuyers([FromQuery] GetBuyersQuery getBuyersQuery)
+        {
+            var result = await _mediator.Send(getBuyersQuery);
+            return Ok(result);
+        }
+    }
+}
