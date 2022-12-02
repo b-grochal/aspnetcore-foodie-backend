d0265ad620076de792efb7aba733d0a727619b78
Bartosz Grochal <radu480@outlook.com>
2022-12-02 23:00:40 +0100

    Modify property of auditable command.

--- Foodie.Shared/Commands/AuditableCreateCommand.cs
+++ Foodie.Shared/Commands/AuditableCreateCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Foodie.Shared.Commands
 {
     public abstract class AuditableCreateCommand
     {
+        [IgnoreDataMember]
         public string CreatedBy { get; set; }
     }
 }
