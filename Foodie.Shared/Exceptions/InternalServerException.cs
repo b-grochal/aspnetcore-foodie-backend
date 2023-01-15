75a1d5bf851f49c4bd6ee517aaa5dcd27c84b465
Bartosz Grochal <radu480@outlook.com>
2023-01-15 13:23:56 +0100

    #47 | Prepared exception for internal server errors.

---
+++ Foodie.Shared/Exceptions/InternalServerException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodie.Shared.Exceptions
+{
+    public abstract class InternalServerException : Exception
+    {
+        protected InternalServerException(string message) : base(message) { }
+    }
+}
