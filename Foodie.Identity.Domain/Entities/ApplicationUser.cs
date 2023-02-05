7d7bae2b1bdc6a360288ac9711078c5cb78bd845
Bartosz Grochal <radu480@outlook.com>
2023-02-05 18:41:56 +0100

    Modify domain of identity service.

--- Foodie.Identity.Domain/Entities/ApplicationUser.cs
+++ Foodie.Identity.Domain/Entities/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Foodie.Shared.Entities;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,9 @@ namespace Foodie.Identity.Domain.Entities
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTimeOffset? LastModifiedDate { get; set; }
     }
 }
--- Foodie.Identity.Domain/Entities/OrderHandler.cs
+++ Foodie.Identity.Domain/Entities/OrderHandler.cs
@@ -8,5 +8,6 @@ namespace Foodie.Identity.Domain.Entities
 {
     public class OrderHandler : ApplicationUser
     {
+        public int LocationId { get; set; }
     }
 }
