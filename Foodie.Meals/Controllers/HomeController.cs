4dde3e5d945ad75fd69641f970188ba2e1f1c880
Bartosz Grochal <radu480@outlook.com>
2023-04-02 20:46:02 +0200

    Add home controllers to the rest of the services.

---
+++ Foodie.Meals/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.Meals.API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        // GET
+        [HttpGet]
+        public IActionResult GetServiceName()
+        {
+            return Ok("Foodie Meals Service");
+        }
+    }
+}
---
+++ Foodie.Orders.API/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.Orders.API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        // GET
+        [HttpGet]
+        public IActionResult GetServiceName()
+        {
+            return Ok("Foodie Orders Service");
+        }
+    }
+}
