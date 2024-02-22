ab1176d28063d35ddbc4c24b4d69ed941890f9b3
Bartosz Grochal <radu480@outlook.com>
2024-02-22 19:22:57 +0100

    Register unit of work.

--- Foodie.Orders.Infrastructure/InfrastructureInstaller.cs
+++ Foodie.Orders.Infrastructure/InfrastructureInstaller.cs
@@ -1,19 +1,16 @@
-﻿using Foodie.Orders.Application.Contracts.Infrastructure.Queries.Orders;
+﻿using Foodie.Common.Application.Contracts.Infrastructure.Database;
+using Foodie.Common.Infrastructure.Database;
+using Foodie.Common.Infrastructure.Database.Interfaces;
 using Foodie.Orders.Application.Contracts.Infrastructure.Queries.Buyers;
 using Foodie.Orders.Application.Contracts.Infrastructure.Queries.Contractors;
+using Foodie.Orders.Application.Contracts.Infrastructure.Queries.Orders;
 using Foodie.Orders.Application.Contracts.Infrastructure.Repositories;
 using Foodie.Orders.Infrastructure.Contexts;
 using Foodie.Orders.Infrastructure.Queries;
 using Foodie.Orders.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Foodie.Orders.Infrastructure
 {
@@ -23,8 +20,12 @@ namespace Foodie.Orders.Infrastructure
         {
             services.AddDbContext<OrdersDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DbConnection")));
+
+            services.AddScoped<IDbContext, OrdersDbContext>();
+
             services.AddSingleton<DapperContext>();
 
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IOrdersRepository, OrdersRepository>();
             services.AddScoped<IBuyersRepository, BuyersRepository>();
             services.AddScoped<IContractorsRepository, ContractorsRepository>();
