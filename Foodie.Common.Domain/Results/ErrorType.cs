f280940666b34847be18984e3b083beef887a1e1
Bartosz Grochal <radu480@outlook.com>
2024-03-08 20:20:29 +0100

    Prepare basic types for result pattern.

---
+++ Foodie.Common.Domain/Results/ErrorType.cs
@@ -0,0 +1,10 @@
+﻿namespace Foodie.Common.Domain.Results
+{
+    public enum ErrorType
+    {
+        Failure = 0,
+        Validation = 1,
+        NotFound = 2,
+        Conflict = 3
+    }
+}
---
+++ Foodie.Common.Domain/Results/Result.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Foodie.Common.Domain.Results
+{
+    public class Result
+    {
+        protected Result(bool isSucess, Error error)
+        {
+            if(isSucess && error != Error.None ||
+                !isSucess && error == Error.None)
+            {
+                throw new ArgumentException("Invalid error", nameof(error));
+            }
+
+            IsSucess = isSucess;
+            Error = error;
+        }
+
+        public bool IsSucess { get; }
+
+        public bool IsFailure => !IsSucess;
+
+        public Error Error { get; }
+
+        public static Result Success() => new (true, Error.None);
+
+        public static Result Failure(Error error) => new (false, error);
+    }
+
+    public class Result<TValue> : Result
+    {
+        private readonly TValue? _value;
+
+        protected Result(TValue? value, bool isSuccess, Error error) : base(isSuccess, error)
+        {
+            _value = value;
+        }
+
+        public static Result<TValue> Success<TValue>(TValue value) => new (value, true, Error.None);
+
+        public static Result<TValue> Failure<TValue>(Error error) => new (default, false, error);
+
+        public TValue? Value => IsSucess ? _value! : throw new InvalidOperationException("The value of a failure result can't be accessed");
+
+        public static implicit operator Result<TValue>(TValue? value) =>
+            value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+    }
+}
