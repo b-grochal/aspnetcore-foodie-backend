0d96dbff42326811d3dc1db9f34d668fa177c7cc
Bartosz Grochal <radu480@outlook.com>
2024-04-04 20:35:56 +0200

    Refactor program.cs of meals.

--- Foodie.Meals/Program.cs
+++ Foodie.Meals/Program.cs
@@ -1,35 +1,110 @@
+//using Foodie.Common.Api.Settings;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Hosting;
+//using Serilog;
+
+//namespace Foodie.Meals
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            IConfigurationRoot configuration = new ConfigurationBuilder()
+//                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+//                .Build();
+
+//            Log.Logger = new LoggerConfiguration()
+//                .ReadFrom
+//                .Configuration(configuration)
+//                .CreateLogger();
+
+//            CreateHostBuilder(args).Build().Run();
+//        }
+
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//                .ConfigureWebHostDefaults(webBuilder =>
+//                {
+//                    webBuilder
+//                    .UseStartup<Startup>();
+//                })
+//                .AddApplicationSettings(SettingsType.JwtToken, SettingsType.Cache)
+//                .UseSerilog();
+//    }
+//}
+
+using Foodie.Common.Api.Middlewares;
 using Foodie.Common.Api.Settings;
+using Foodie.Common.Application.Behaviours;
+using Foodie.Common.Infrastructure.Authentication;
+using Foodie.Meals.API.Grpc;
+using Foodie.Meals.Application;
+using Foodie.Meals.Infrastructure;
+using Foodie.Shared.Behaviours;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
-namespace Foodie.Meals
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom
+    .Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.AddApplicationSettings();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddMealsApplication();
+builder.Services.AddMealsInfrastructure(builder.Configuration);
+builder.Services.ConfigureApplicationSettings(builder.Configuration, SettingsType.JwtToken);
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Foodie.Meals", Version = "v1" });
+});
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom
-                .Configuration(configuration)
-                .CreateLogger();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddGrpc();
 
-            CreateHostBuilder(args).Build().Run();
-        }
+var app = builder.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                    .UseStartup<Startup>();
-                })
-                .AddApplicationSettings(SettingsType.JwtToken, SettingsType.Cache)
-                .UseSerilog();
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foodie.Meals v1"));
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGrpcService<MealsGrpcService>();
+app.MapControllers();
+
+//app.UseSerilog();
+
+app.Run();
+
--- Foodie.Meals/Startup.cs
+++ Foodie.Meals/Startup.cs
@@ -1,77 +1,77 @@
-using Foodie.Common.Api.Middlewares;
-using Foodie.Common.Api.Settings;
-using Foodie.Common.Application.Behaviours;
-using Foodie.Common.Infrastructure.Authentication;
-using Foodie.Meals.API.Grpc;
-using Foodie.Meals.Application;
-using Foodie.Meals.Infrastructure;
-using Foodie.Shared.Behaviours;
-using MediatR;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Serilog;
+//using Foodie.Common.Api.Middlewares;
+//using Foodie.Common.Api.Settings;
+//using Foodie.Common.Application.Behaviours;
+//using Foodie.Common.Infrastructure.Authentication;
+//using Foodie.Meals.API.Grpc;
+//using Foodie.Meals.Application;
+//using Foodie.Meals.Infrastructure;
+//using Foodie.Shared.Behaviours;
+//using MediatR;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.OpenApi.Models;
+//using Serilog;
 
-namespace Foodie.Meals
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+//namespace Foodie.Meals
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
 
-        public IConfiguration Configuration { get; }
+//        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMealsApplication();
-            services.AddMealsInfrastructure(Configuration);
-            services.ConfigureApplicationSettings(Configuration, SettingsType.JwtToken);
-            services.AddJwtAuthentication(Configuration);
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddMealsApplication();
+//            services.AddMealsInfrastructure(Configuration);
+//            services.ConfigureApplicationSettings(Configuration, SettingsType.JwtToken);
+//            services.AddJwtAuthentication(Configuration);
+//            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+//            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Foodie.Meals", Version = "v1" });
-            });
+//            services.AddControllers();
+//            services.AddSwaggerGen(c =>
+//            {
+//                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Foodie.Meals", Version = "v1" });
+//            });
 
-            services.AddAutoMapper(typeof(Startup));
-            services.AddGrpc();
-        }
+//            services.AddAutoMapper(typeof(Startup));
+//            services.AddGrpc();
+//        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foodie.Meals v1"));
-            }
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//                app.UseSwagger();
+//                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foodie.Meals v1"));
+//            }
 
-            app.UseMiddleware<ExceptionMiddleware>();
+//            app.UseMiddleware<ExceptionMiddleware>();
 
-            app.UseSerilogRequestLogging();
+//            app.UseSerilogRequestLogging();
 
-            app.UseHttpsRedirection();
+//            app.UseHttpsRedirection();
 
-            app.UseRouting();
+//            app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+//            app.UseAuthentication();
+//            app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<MealsGrpcService>();
-                endpoints.MapControllers();
-            });
-        }
-    }
-}
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapGrpcService<MealsGrpcService>();
+//                endpoints.MapControllers();
+//            });
+//        }
+//    }
+//}
