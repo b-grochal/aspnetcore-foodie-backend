8d8103a886a795a995cca66700dcf35807c02713
Bartosz Grochal <radu480@outlook.com>
2024-05-22 20:29:54 +0200

    Prepare methods for getting audits for entities.

--- Foodie.Common.Infrastructure/Database/Contexts/BaseDbContext.cs
+++ Foodie.Common.Infrastructure/Database/Contexts/BaseDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using Foodie.Common.Infrastructure.Database.Audits;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 
 namespace Foodie.Common.Infrastructure.Database.Contexts
 {
@@ -7,9 +11,82 @@ namespace Foodie.Common.Infrastructure.Database.Contexts
     {
         public DbSet<Audit> Audits { get; set; }
 
-        public void PrepareAuditsForEntities()
+        public void PrepareAuditsForEntities(int applicationUserId, string applicationUserEmail)
         {
-            
+            var modifiedEntites = ChangeTracker
+                .Entries()
+                .Where(e => e.State == EntityState.Added
+                || e.State == EntityState.Modified
+                || e.State == EntityState.Deleted)
+                .ToList();
+
+            var audits = new List<Audit>();
+
+            foreach (var modifiedEntity in modifiedEntites)
+            {
+                var audit = new Audit
+                {
+                    TableName = modifiedEntity.Entity.GetType().Name,
+                    ApplicationUserId = applicationUserId,
+                    ApplicationUserEmail = applicationUserEmail
+                };
+
+                switch(modifiedEntity.State)
+                {
+                    case EntityState.Added:
+                        audit.Type = AuditType.Create;
+                        audit.NewValues = GetNewValues(modifiedEntity);
+                        break;
+                    case EntityState.Modified:
+                        audit.Type = AuditType.Update;
+                        audit.NewValues = GetNewValues(modifiedEntity);
+                        audit.OldValues = GetOldValues(modifiedEntity);
+                        audit.ModifiedColumns = GetChangedColumns(modifiedEntity);
+                        break;
+                    case EntityState.Deleted:
+                        audit.Type = AuditType.Delete;
+                        audit.OldValues = GetOldValues(modifiedEntity);
+                        break;
+                }
+            }
+
+            Audits.AddRangeAsync(audits);
+        }
+
+        private string GetOldValues(EntityEntry entityEntry)
+        {
+            var oldValues = new Dictionary<string, object>();
+
+            foreach(var property in entityEntry.Properties)
+            {
+                oldValues[property.Metadata.Name] = property.OriginalValue;
+            }
+
+            return oldValues.Count == 0 ? null : JsonSerializer.Serialize(oldValues);
+        }
+
+        private string GetNewValues(EntityEntry entityEntry)
+        {
+            var newValues = new Dictionary<string, object>();
+
+            foreach (var property in entityEntry.Properties)
+            {
+                newValues[property.Metadata.Name] = property.CurrentValue;
+            }
+
+            return newValues.Count == 0 ? null : JsonSerializer.Serialize(newValues);
+        }
+
+        private string GetChangedColumns(EntityEntry entityEntry)
+        {
+            var changedCloumns = new List<string>();
+
+            foreach (var property in entityEntry.Properties.Where(p => p.IsModified))
+            {
+                changedCloumns.Add(property.Metadata.Name);
+            }
+
+            return changedCloumns.Count == 0 ? null : JsonSerializer.Serialize(changedCloumns);
         }
     }
 }
