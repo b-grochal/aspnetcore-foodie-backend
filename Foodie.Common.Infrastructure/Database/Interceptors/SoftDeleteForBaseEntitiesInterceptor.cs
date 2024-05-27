079cdb499dc50853bbed733a2a41e594ca19829a
Bartosz Grochal <radu480@outlook.com>
2024-05-27 19:51:34 +0200

    Prepare interceptors for soft deletes.

---
+++ Foodie.Common.Infrastructure/Database/Interceptors/SoftDeleteForBaseEntitiesInterceptor.cs
@@ -0,0 +1,41 @@
+﻿using Foodie.Common.Domain.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Common.Infrastructure.Database.Interceptors
+{
+    public class SoftDeleteForBaseEntitiesInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default)
+        {
+            if (eventData.Context is null)
+            {
+                return base.SavingChangesAsync(
+                eventData, result, cancellationToken);
+            }
+
+            IEnumerable<EntityEntry<ISoftDeletableBaseEntity>> entries =
+            eventData
+            .Context
+            .ChangeTracker
+            .Entries<ISoftDeletableBaseEntity>()
+            .Where(e => e.State == EntityState.Deleted);
+
+            foreach (EntityEntry<ISoftDeletableBaseEntity> softDeletable in entries)
+            {
+                softDeletable.State = EntityState.Modified;
+                softDeletable.Entity.IsDeleted = true;
+            }
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
---
+++ Foodie.Common.Infrastructure/Database/Interceptors/SoftDeleteForDomainEntitiesInterceptor.cs
@@ -0,0 +1,41 @@
+﻿using Foodie.Common.Domain.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Foodie.Common.Infrastructure.Database.Interceptors
+{
+    public class SoftDeleteForDomainEntitiesInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default)
+        {
+            if (eventData.Context is null)
+            {
+                return base.SavingChangesAsync(
+                eventData, result, cancellationToken);
+            }
+
+            IEnumerable<EntityEntry<ISoftDeletableDomainEntity>> entries =
+            eventData
+            .Context
+            .ChangeTracker
+            .Entries<ISoftDeletableDomainEntity>()
+            .Where(e => e.State == EntityState.Deleted);
+
+            foreach (EntityEntry<ISoftDeletableDomainEntity> softDeletable in entries)
+            {
+                softDeletable.State = EntityState.Modified;
+                softDeletable.Entity.MarkAsDeleted();
+            }
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
